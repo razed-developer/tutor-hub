dc95e150a3087a6050d27de832c4adc0ff8bfee8
razed-developer <razed-developer@outlook.com>
2026-08-06 07:51:15 -0700

    Fix react-katex TypeScript module declaration

--- src/react-katex.d.ts
+++ src/react-katex.d.ts
@@ -1,12 +1,1 @@
-declare module 'react-katex' {
-  import type { ComponentType, HTMLAttributes } from 'react'
-
-  export type MathComponentProps = HTMLAttributes<HTMLElement> & {
-    math: string
-    errorColor?: string
-    renderError?: (error: Error) => React.ReactNode
-  }
-
-  export const BlockMath: ComponentType<MathComponentProps>
-  export const InlineMath: ComponentType<MathComponentProps>
-}
+declare module 'react-katex'
