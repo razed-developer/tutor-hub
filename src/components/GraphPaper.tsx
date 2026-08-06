c6d8b94d28c673a46a9d9c394338e2ff1b240a4c
razed-developer <razed-developer@outlook.com>
2026-08-06 07:46:01 -0700

    Add coordinate grid component

---
+++ src/components/GraphPaper.tsx
@@ -0,0 +1,8 @@
+export default function GraphPaper() {
+  return (
+    <div className="graph-paper" aria-label="Coordinate grid">
+      <div className="axis axis-x" />
+      <div className="axis axis-y" />
+    </div>
+  )
+}
